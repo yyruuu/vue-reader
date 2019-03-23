11b781a687292f2e4262784afa00bf1b9b54db35
Yurong Yang <1103084176@qq.com>
2019-03-23 23:05:13 +0800

    vue reader

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+document.addEventListener('DOMContentLoaded',()=>{
+  //找到根元素
+  const html = document.querySelector('html');
+  //当屏幕的宽度很大时，字体也会很大 所以要对字体的大小做一个限制
+  let fontSize = window.innerWidth / 10;
+  //三元运算符，限制fontSize的值最大为50
+  fontSize = fontSize > 50 ? 50 : fontSize;
+  //将跟元素的fontSize值设置为屏幕的十分之一
+  html.style.fontSize = fontSize + 'px';
+})
+</script>
+
+<style>
+
+</style>
---
+++ src/components/TitleBar.vue
@@ -0,0 +1,70 @@
+<template>
+  <transition name="slide-down"> 
+    <div class="title-wrapper" v-show="ifTitleAndMenuShow">
+      <div class="left">
+        <span class="icon-back icon"></span>
+      </div>
+      <div class="right">
+        <div class="icon-wrapper">
+          <span class="icon-cart icon"></span>
+          <span class="icon-person icon"></span>
+          <span class="icon-more icon"></span>
+        </div>
+      </div>
+    </div>
+   </transition>
+</template>
+
+<script>
+export default {
+  props:{
+    //接收Ebook.vue中的变量ifTitleAndMenuShow
+    ifTitleAndMenuShow:{
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import '../assets/styles/global';
+
+  .title-wrapper{
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: px2rem(48);
+    display: flex;
+    z-index: 101;
+    background-color: white;
+    box-shadow: 0 px2rem(8) px2rem(8) rgba(0,0,0, .15);
+
+    .left{
+      flex: 0 0 px2rem(60);
+      @include center;
+    }
+    .right{
+      flex: 1;
+      display: flex;
+      //居右排列
+      justify-content: flex-end;
+      .icon-wrapper{
+        flex: 0 0 px2rem(40);
+        @include center;
+        .icon-cart{
+          font-size: px2rem(22);
+          margin-right: px2rem(20);
+        }
+        .icon-person{
+          margin-right: px2rem(20);
+        }
+        .icon-more{
+          margin-right: px2rem(20);
+        }
+      }
+    }
+  
+  }
+</style>
